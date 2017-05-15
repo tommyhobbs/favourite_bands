26d98d6c7ee956ffb38f8eae9e6dba5ab67f8044
Tom Hobbs <Tom.Hobbs@uk.ibm.com>
2017-05-15 11:15:57 +0100

    first react component

--- src/client.js
+++ src/client.js
@@ -1,1 +1,7 @@
-console.log("Hello Someone Else");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Movie from './components/Movie';
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<Movie />, app);
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,2 +1,7 @@
 import React from 'react';
 
+function Movie(){
+    return <li> Toy Story (1995)</li>
+}
+
+export default Movie;
