49e64d07ee1fee36723376aac7d179d8496bce29
Tom Hobbs <Tom.Hobbs@uk.ibm.com>
2017-05-15 11:03:46 +0100

    initial commit

---
+++ src/client.js
@@ -0,0 +1,1 @@
+console.log("Hello Someone Else");
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    context: path.join(__dirname, "src"),
+    devtool: false,
+    entry: "./client.js",
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-0'],
+                    plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
+                }
+            }
+        ]
+    },
+    output: {
+        path: path.join(__dirname, "src"),
+        filename: "client.min.js"
+    },
+    plugins: [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false })
+    ]
+};
